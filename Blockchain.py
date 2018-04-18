4bfff6c5afa000dca99a4b0e0a224f9221e635e9
JasonYichongXing <37820014+JasonYichongXing@users.noreply.github.com>
2018-04-18 00:11:55 -0400

    Update Blockchain.py

--- Blockchain.py
+++ Blockchain.py
@@ -27,9 +27,13 @@ class Block:
         self.previous_hash = INIT_PREV_HASH
         self.Nonce = INIT_NNONCE
         self.Info = Transaction_info
-       
-        self.Hash = self.GetHash()   
-
+        #self.Hash = self.GetHash()    ->Using @Property instead
+        
+    @property            # a special type of attribute that calls a custom getter function when getting the value, 
+                         # so can make it dynamically update the value
+    def Hash(self): 
+        return self.GetHash()   
+        
         
     def GetHash(self):
         h = hashlib.sha256()
@@ -43,13 +47,13 @@ class Block:
         
     
     def __str__(self):
-        return '\nBlock No: ' + str(self.BlockNo) + '\nBlock Hash: ' + str(self.GetHash()) + '\nPrevius Hash: ' + str(self.previous_hash) + '\nNonce: ' + str(self.Nonce) + '\nTransaction Info: ' + str(self.Info) + '\n============\n'
+        return '\nBlock No: ' + str(self.BlockNo) + '\nBlock Hash: ' + str(self.Hash) + '\nPrevius Hash: ' + str(self.previous_hash) + '\nNonce: ' + str(self.Nonce) + '\nTransaction Info: ' + str(self.Info) + '\n============\n'
 
 ####################################
         
 def proof_of_work(block, Diff = INIT_DIFFICULTY):
     Diff_string = ''.join(['0' for _ in range(Diff)])
-    return block.GetHash()[:Diff] == Diff_string
+    return block.Hash[:Diff] == Diff_string
 
 
 def BlockChain_gen(N):
@@ -60,33 +64,29 @@ def BlockChain_gen(N):
     BlockChain.append(Genesis)
     
     assert proof_of_work(Genesis) == False or INIT_DIFFICULTY ==0
-    assert proof_of_work(Genesis, 0) == True 
 
     for n in range(1, N+1):
         Block_add = Block('Block #' + str(n))
         Block_add.BlockNo = n
-        Block_add.previous_hash = BlockChain[-1].GetHash()
+        Block_add.previous_hash = BlockChain[-1].Hash
+        
+        while not proof_of_work(Block_add) and Block_add.Nonce < MAX_NONCE:
+            Block_add.Nonce += 1
     
-        i = 1
-        while i < MAX_NONCE:
-            if proof_of_work(Block_add):
-                BlockChain.append(Block_add)
-                break
-            
-            else:
-                Block_add.Nonce += 1      
-            i += 1
+        BlockChain.append(Block_add)
+
     return BlockChain
 
-'''
-for _ in BlockChain_gen(10):
+#####################################3
+#Test:    
+
+N = 20    
+
+Example = BlockChain_gen(N)
+
+for _ in BlockChain_gen(N):
+    print(_.BlockNo, ': ', _.Hash)
+
+for _ in BlockChain_gen(N):
     print(_)
-'''
-    
-x = BlockChain_gen(20)
-
-for _ in BlockChain_gen(20):
-    print(_.BlockNo, ': ', _.GetHash())
-
-
 
